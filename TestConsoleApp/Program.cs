f697f13ee6af0f7de05dca1114723a0164f50cb2
Patrick Creech <pcreech@redhat.com>
2017-08-01 10:45:48 -0400

    Initial Commit
    Initial Commit

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
